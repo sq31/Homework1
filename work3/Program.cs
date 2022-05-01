8b24782c88a375045edaf6e88310b62ee60809d2
OstapchukSergey <sq31@mail.ru>
2022-05-01 21:50:45 +0300

    task3

--- work3/Program.cs
+++ work3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(result);
+if(number%2==0)  
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+Console.WriteLine("Число является нечетным");
+}
