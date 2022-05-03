0db979591724c4e06b3d01cef082ce20e748f4ce
OstapchukSergey <sq31@mail.ru>
2022-05-03 11:57:50 +0300

    Деление на 3

---
+++ work5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, делится ли число на 3 без остатка
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%3==0)  
+{
+    Console.WriteLine("Число делится на 3");
+}
+else
+{
+Console.WriteLine("Число не делится на 3");
+}
